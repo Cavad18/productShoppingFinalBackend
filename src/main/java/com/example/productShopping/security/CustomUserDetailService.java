31a2e31b353dab58b33fab740a005bd89daaef51
Ваше имя < email>
2025-10-27 12:20:23 -0700

    add CustomUserDetailService

---
+++ src/main/java/com/example/productShopping/security/CustomUserDetailService.java
@@ -0,0 +1,34 @@
+package com.example.productShopping.security;
+
+import com.example.productShopping.entity.User;
+import com.example.productShopping.repository.ProductRepository;
+import com.example.productShopping.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username:" + username)
+                );
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
