c523c476d08d1927d763f04e0737225c11e53d26
Ваше имя < email>
2025-10-31 02:57:32 -0700

    add Dto

---
+++ src/main/java/com/example/productShopping/dto/UserProfileDto.java
@@ -0,0 +1,18 @@
+package com.example.productShopping.dto;
+
+import lombok.Data;
+
+@Data
+public class UserProfileDto {
+    private String name;
+    private String surname;
+    private String email;
+    private String username;
+
+    public UserProfileDto(String name, String surname, String email, String username) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.username = username;
+    }
+}
